61f6d5fb76b9b935a4d79a4eef464c5d24aede6d
liusheng <liushenggcs@163.com>
2021-07-28 01:05:53 +0800

    T5_Of05_3 空格替换：原地修改

--- Offer/src/t4_String/Offer05.java
+++ Offer/src/t4_String/Offer05.java
@@ -1,6 +1,7 @@
 package t4_String;
 
 public class Offer05 {
+	// create array O(n)
 	public String replaceSpace(String s) {
 		int length = s.length();
 		char[] array = new char[3 * length];
@@ -19,6 +20,7 @@ public class Offer05 {
 		return newStr;
     }
 	
+	// s.toCharArray() O(n)
 	public String replaceSpace2(String s) {
 		StringBuilder res = new StringBuilder();
 		for(char c : s.toCharArray()) {
@@ -29,4 +31,37 @@ public class Offer05 {
 		}
 		return res.toString();
     }
+	
+	// C++
+	/**
+	public:
+    string replaceSpace(string s) {
+        int count = 0, len = s.size();
+        // 统计空格数量
+//        int k = 0;
+//        while(s[k] != '\0') {
+//            if (s[k] == ' ') count++;
+//            k ++;
+//        }
+	     for(char c : s) {
+	     	if (c == ' ') count++;
+	     }
+	     
+        // 修改 s 长度
+        s.resize(len + 2 * count); // 用封装好的函数，防止长度越界，
+        // 倒序遍历修改
+        int i = len, j = s.size();
+        while(i < j && i >= 0) {
+            if (s[i] != ' ')
+                s[j --] = s[i];
+            else {
+                s[j --] = '0';
+                s[j --] = '2';
+                s[j --] = '%';
+            }
+            i --;
+        }
+        return s;
+    }
+	 */
 }
