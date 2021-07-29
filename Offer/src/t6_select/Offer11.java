76db4233e5221991c71a6cd1e3d12002973a6539
liusheng <liushenggcs@163.com>
2021-07-30 00:52:11 +0800

    11_solution4 旋转数组的最小数字

--- Offer/src/t6_select/Offer11.java
+++ Offer/src/t6_select/Offer11.java
@@ -9,12 +9,18 @@ public class Offer11 {
 		while(left < right) { // 不能相等，否则一直死循环
 			int mid = (left + right) >> 1;
 		
+			// 之所以与最右边比较，因为初始状态转折点为0时，right就在转折点的右边
 			if(numbers[mid] > numbers[right]) { // 排除左边
 				left = mid + 1;
 			} else  if(numbers[mid] < numbers[right]){ // 排除右边
 				right = mid;
-			} else {
-				right --;
+			} else { // 因只有一个转折，mid左边或是右边值全相等，或是所有值相等，可以用线性查找
+				int x = left;
+                for(int k = left + 1; k < right; k++) {
+                    if(numbers[k] < numbers[x]) x = k;
+                }
+                return numbers[x];
+
 			}
 		}
 		
