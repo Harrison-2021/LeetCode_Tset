81ff373c8cf04cf304a4616c1d0a441736958a58
liusheng <liushenggcs@163.com>
2021-07-30 00:24:11 +0800

    11_solution2 旋转数组的最小数字

--- Offer/src/t6_select/Offer11.java
+++ Offer/src/t6_select/Offer11.java
@@ -4,25 +4,20 @@ public class Offer11 {
 	// 最小值两个特定：1.比左边值小，
 	// 2，在最小值右侧元素，一定<=right,最小值左边元素，一定>=right
 	public int minArray(int[] numbers) {
-		if(numbers.length == 1) return numbers[0];
-		
 		int left = 0, right = numbers.length -1;
 		
-		while(left <= right) {
+		while(left < right) { // 不能相等，否则一直死循环
 			int mid = (left + right) >> 1;
-			if(mid == 0) {
-				return numbers[mid] < numbers[mid + 1] ? numbers[mid] : numbers[mid + 1];
-			} else if(numbers[mid] < numbers[mid -1]) {
-				return numbers[mid];
-			} else if(numbers[mid] > numbers[right]) { // 排除左边
+		
+			if(numbers[mid] > numbers[right]) { // 排除左边
 				left = mid + 1;
 			} else  if(numbers[mid] < numbers[right]){ // 排除右边
-				right = mid -1;
+				right = mid;
 			} else {
 				right --;
 			}
 		}
 		
-		return numbers[0];
+		return numbers[left];
     }
 }
