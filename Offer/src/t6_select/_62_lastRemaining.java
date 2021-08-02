b00374404cc4e48ff7a77a04f645636cc8b269a0
liusheng <liushenggcs@163.com>
2021-08-03 00:13:59 +0800

    62_solution2 圆圈中最后剩下的数字
    迭代法

--- Offer/src/t6_select/_62_lastRemaining.java
+++ Offer/src/t6_select/_62_lastRemaining.java
@@ -19,4 +19,13 @@ public class _62_lastRemaining {
 		int index = f(n -1, m);
 		return (m + index) % n;
 	}
+	
+	// 迭代法 O(1)空间
+	public int lastRemaining2(int n, int m) {
+		int f = 0;
+		for(int i = 2; i < n + 1; i ++) { // 循环终止条件从前几个数中找规律
+			f = (m + f) % i;
+		}
+		return f;
+	}
 }
