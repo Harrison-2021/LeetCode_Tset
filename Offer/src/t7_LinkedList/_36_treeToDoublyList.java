d5bfc20a0564800c2cfaaa7841b3ac528d3ba444
liusheng <liushenggcs@163.com>
2021-08-02 23:44:29 +0800

    36_solution 二叉搜索树与双向链表

---
+++ Offer/src/t7_LinkedList/_36_treeToDoublyList.java
@@ -0,0 +1,58 @@
+package t7_LinkedList;
+
+public class _36_treeToDoublyList {
+	// Definition for a Node.
+	class Node {
+	    public int val;
+	    public Node left;
+	    public Node right;
+
+	    public Node() {}
+
+	    public Node(int _val) {
+	        val = _val;
+	    }
+
+	    public Node(int _val,Node _left,Node _right) {
+	        val = _val;
+	        left = _left;
+	        right = _right;
+	    }
+	};
+
+	// 确定需要的指针，返回的头节点 head, 当前节点curPit(局部变量), 前一个节点prePit
+	private Node prePit = null, head = null;
+	
+    public Node treeToDoublyList(Node root) {
+        // 特殊情况先排除
+    	if(root == null) return null;
+    	
+    	// 递归中序遍历，查找到链表中的每个元素，将链表left，right指向定义好
+    	inOrder(root);
+    	
+    	// 为了构建循环列表，将首尾相连
+    	head.left = prePit;
+    	prePit.right = head;
+    	
+    	return head;
+    }
+    
+    private void inOrder(Node curPit) {
+    	if(curPit == null) return;
+    	
+    	inOrder(curPit.left);
+    	
+    	// 将当前找到的node与前一个节点构建成双向链表
+    	if(prePit == null) {
+    		head = curPit;
+    	} else {
+    		prePit.right = curPit;
+    	}
+    	curPit.left = prePit;
+    	
+    	// 维护prePid，以便与下个节点构成链表
+    	prePit = curPit;
+    	
+    	inOrder(curPit.right);
+    }
+}
