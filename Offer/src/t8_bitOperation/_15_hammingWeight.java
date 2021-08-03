38c8fb9072b607a4c92a27f7deab5d27e5442e04
liusheng <liushenggcs@163.com>
2021-08-03 21:29:33 +0800

    15_solution3 二进制中1的个数

--- Offer/src/t8_bitOperation/_15_hammingWeight.java
+++ Offer/src/t8_bitOperation/_15_hammingWeight.java
@@ -31,4 +31,14 @@ public class _15_hammingWeight {
 		 }
 		 return count;
 	 }
+	 
+	 public int hammingWeight3(int n) {
+		 int count = 0;
+		 while(n != 0) {
+			 n = n & (n -1);
+			 n = 0xffffffff & n;
+			 count ++;
+		 }
+		 return count;
+	 }
 }
