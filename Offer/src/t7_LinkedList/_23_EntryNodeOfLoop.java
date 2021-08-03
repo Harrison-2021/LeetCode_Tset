88090687e2dbc8fb8c9cecc4216db63ef2eaea7d
liusheng <liushenggcs@163.com>
2021-08-03 14:03:57 +0800

    23_solution2 链表中环的入口结点
    快慢双指针，拉开距离，再根据距离进行逼近找到目标节点

--- Offer/src/t7_LinkedList/_23_EntryNodeOfLoop.java
+++ Offer/src/t7_LinkedList/_23_EntryNodeOfLoop.java
@@ -33,7 +33,33 @@ public class _23_EntryNodeOfLoop {
         
         return null;
     }
+	
+	// 快慢双指针，拉开距离，再根据距离进行逼近找到目标节点
+	public ListNode EntryNodeOfLoop2(ListNode pHead) {
+		//if(pHead == null) return null;
 		
+		ListNode fast = pHead;
+		ListNode slow = pHead;
+		
+		// 找到第一次相遇点
+		while(fast != null && fast.next != null) { // 相等不能作为循环终止条件，因为初始旧相等
+			fast = fast.next.next;
+			slow = slow.next;
+			
+			if(fast == slow) break; // 用break也可终止循环
+		}
+		// 排除不是环形链表情况
+		if(fast == null || fast.next == null) return null;
+		
+		// s = m + (n -1)*(m + d),从头节点，相遇点两头逼近，一定会在目标节点汇合
+		fast = pHead;
+		while(fast != slow) {
+			fast = fast.next;
+			slow = slow.next;
+		}
+		
+		return fast;
+	}
 }
 
 
