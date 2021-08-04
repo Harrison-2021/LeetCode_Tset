5fa9f99d48144dab486198233eaeaa2d3341b09f
liusheng <liushenggcs@163.com>
2021-08-04 17:23:06 +0800

    39_solution2 数组中出现次数超过一半的数字

--- Offer/src/t3_Array/_39_majorityElement.java
+++ Offer/src/t3_Array/_39_majorityElement.java
@@ -22,4 +22,29 @@ public class _39_majorityElement {
 		 }
 		 return 0;
 	 }
+		 // 直接找到最大的哈希键值，减少时间
+	 private Map<Integer, Integer> countNums(int[] nums) {
+	        Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
+	        for (int num : nums) {
+	            if (!counts.containsKey(num)) {
+	                counts.put(num, 1);
+	            } else {
+	                counts.put(num, counts.get(num) + 1);
+	            }
+	        }
+	        return counts;
+	    }
+
+    public int majorityElement2(int[] nums) {
+        Map<Integer, Integer> counts = countNums(nums);
+
+        Map.Entry<Integer, Integer> majorityEntry = null;
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
+                majorityEntry = entry;
+            }
+        }
+
+        return majorityEntry.getKey();
+    }
 }
