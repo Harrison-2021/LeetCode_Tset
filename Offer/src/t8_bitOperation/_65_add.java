38b91a883fa14fbe92f76fb28a9544446010994c
liusheng <liushenggcs@163.com>
2021-08-04 12:09:03 +0800

    65_solution1 不用加减乘除做加法

---
+++ Offer/src/t8_bitOperation/_65_add.java
@@ -0,0 +1,20 @@
+package t8_bitOperation;
+
+/**
+ * @author ls2690069470
+ *	Offer 65. 不用加减乘除做加法
+ */
+public class _65_add {
+	 public int add(int a, int b) {
+		 int xorNum = a ^ b;
+		 int andNum = (a & b) << 1;
+		 
+		 while(andNum != 0) {
+			 int tempXor = xorNum ^ andNum;
+			 andNum = (xorNum & andNum) << 1;
+			 xorNum = tempXor;
+		 }
+		 
+		 return xorNum;
+	 }
+}
