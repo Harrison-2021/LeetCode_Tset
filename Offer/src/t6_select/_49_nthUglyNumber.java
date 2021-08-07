50976732c8400a9a4d1d7a54c3dceca4812b74a3
liusheng <liushenggcs@163.com>
2021-08-07 11:32:25 +0800

    49_solution1 丑数
    代码优化

--- Offer/src/t6_select/_49_nthUglyNumber.java
+++ Offer/src/t6_select/_49_nthUglyNumber.java
@@ -15,32 +15,33 @@ public class _49_nthUglyNumber {
 		int p2 = 0, p3 = 0, p5 = 0;	// 2，3，5丑数因子的下标索引，与已知丑数相乘一定还是丑数
 		
 		while(nextUglyIndex < n) {
-			pUgly[nextUglyIndex] = min(pUgly[p2]*2, pUgly[p3]*3, pUgly[p5]*5);
+			int num2 = pUgly[p2]*2, num3 = pUgly[p3]*3, num5 = pUgly[p5]*5;
+			pUgly[nextUglyIndex] = Math.min(Math.min(num2, num3), num5);
 			// 更新丑数临界点，并记住临界点
-			while(pUgly[p2] * 2 <= pUgly[nextUglyIndex]) {
+			if(num2 == pUgly[nextUglyIndex]) {
 				p2 ++; // 找下一个大于已知丑数的，可能的最小丑数
 			}
-			while(pUgly[p3] * 3 <= pUgly[nextUglyIndex]) {
+			if(num3 == pUgly[nextUglyIndex]) {
 				p3 ++;
 			}
-			while(pUgly[p5] * 5 <= pUgly[nextUglyIndex]) {
+			if(num5 == pUgly[nextUglyIndex]) {
 				p5 ++;
 			}
 			
 			nextUglyIndex ++;			
 		}
 		
-		return pUgly[nextUglyIndex -1];
+		return pUgly[n - 1];
 	}
 
-	private int min(int i, int j, int k) {
-		int min = i < j ? i : j;
-		return min < k ? min : k;
-	}
+//	private int min(int i, int j, int k) {
+//		int min = i < j ? i : j;
+//		return min < k ? min : k;
+//	}
 	
 	public static void main(String[] args) {
-		
-		System.out.println(new _49_nthUglyNumber().nthUglyNumber(10));
-
+		for(int i = 1; i <= 11; i++) {
+			System.out.println(new _49_nthUglyNumber().nthUglyNumber(i));
+		}
 	}
 }
