e7640117dbe67fc83312caaeb8c0ed73b2435f56
liusheng <liushenggcs@163.com>
2021-08-07 12:04:36 +0800

    49_solution2 丑数
    使用最小堆，但时间复杂度为比较高一些，额外空间花销也大一些

--- Offer/src/t6_select/_49_nthUglyNumber.java
+++ Offer/src/t6_select/_49_nthUglyNumber.java
@@ -1,5 +1,8 @@
 package t6_select;
 
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
 
 /**
  * @author ls2690069470
@@ -39,9 +42,29 @@ public class _49_nthUglyNumber {
 //		return min < k ? min : k;
 //	}
 	
+	public int nthUglyNumber2(int n) {
+        int[] factors = {2, 3, 5};
+        Set<Long> seen = new HashSet<Long>();
+        PriorityQueue<Long> heap = new PriorityQueue<Long>();
+        seen.add(1L);
+        heap.offer(1L);
+        int ugly = 0;
+        for (int i = 0; i < n; i++) {
+            long curr = heap.poll();
+            ugly = (int) curr;
+            for (int factor : factors) {
+                long next = curr * factor;
+                if (seen.add(next)) {
+                    heap.offer(next);
+                }
+            }
+        }
+        return ugly;
+    }
+	
 	public static void main(String[] args) {
 		for(int i = 1; i <= 11; i++) {
-			System.out.println(new _49_nthUglyNumber().nthUglyNumber(i));
+			System.out.println(new _49_nthUglyNumber().nthUglyNumber2(i));
 		}
 	}
 }
