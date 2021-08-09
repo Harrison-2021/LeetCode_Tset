44c4b7d188c602f751b2a90087227c0ad20eae58
liusheng <liushenggcs@163.com>
2021-08-09 17:35:21 +0800

    26_solution2 树的子结构
    代码简化

--- Offer/src/t9_BSTTree/_26_isSubStructure.java
+++ Offer/src/t9_BSTTree/_26_isSubStructure.java
@@ -15,22 +15,26 @@ public class _26_isSubStructure {
 	}
 
 	public boolean isSubStructure(TreeNode A, TreeNode B) {
-		if(A == null || B == null) return false;
+//		if(A == null || B == null) return false;
+//		
+//		// 注意，A，B本身就是两个树的根节点
+//		// 找到A树中与B树相等的节点，以便进行下一步的逐个节点比较
+//		boolean res = false;
+//		if(A.val == B.val) {
+//			res = recur(A, B);
+//		} 
+//		if(!res) {
+//			res = isSubStructure(A.left, B);
+//		}
+//		if(!res) {
+//			res = isSubStructure(A.right, B);
+//		}
+//		
+//		return res;
 		
-		// 注意，A，B本身就是两个树的根节点
-		// 找到A树中与B树相等的节点，以便进行下一步的逐个节点比较
-		boolean res = false;
-		if(A.val == B.val) {
-			res = recur(A, B);
-		} 
-		if(!res) {
-			res = isSubStructure(A.left, B);
-		}
-		if(!res) {
-			res = isSubStructure(A.right, B);
-		}
-		
-		return res;
+		// 注意顺序
+		return (A != null && B != null ) && 
+				(recur(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
 	}
 
 	private boolean recur(TreeNode A, TreeNode B) {
