7466fb73a9812bbdaad8faa43cd66a55dfbc120b
liusheng <liushenggcs@163.com>
2021-08-09 17:18:30 +0800

    26_solution1 树的子结构

---
+++ Offer/src/t9_BSTTree/_26_isSubStructure.java
@@ -0,0 +1,22 @@
+package t9_BSTTree;
+
+import t9_BSTTree._7_buildTree.TreeNode;
+
+/**
+ * @author ls2690069470 Offer 26. 树的子结构
+ */
+public class _26_isSubStructure {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	public boolean isSubStructure(TreeNode A, TreeNode B) {
+		
+	}
+}
