0c9c42fc0c1ca3052f13dec469914ea657b9d19e
liusheng <liushenggcs@163.com>
2021-08-12 09:04:25 +0800

    55_solution2 二叉树的深度
    代码简化以及BFS 层序遍历

--- Offer/src/t9_BSTTree/_55_maxDepth.java
+++ Offer/src/t9_BSTTree/_55_maxDepth.java
@@ -1,5 +1,7 @@
 package t9_BSTTree;
 
+import java.util.*;
+
 /**
  * @author ls2690069470 Offer 55 - I. 二叉树的深度
  */
@@ -13,15 +15,43 @@ public class _55_maxDepth {
 			val = x;
 		}
 	}
-
+	// DFS 后续遍历
 	public int maxDepth(TreeNode root) {
 		if(root == null) {
 			return 0;			
 		}
 		
-		int leftDepth = maxDepth(root.left);
-		int rightDepth = maxDepth(root.right);
+//		int leftDepth = maxDepth(root.left);
+//		int rightDepth = maxDepth(root.right);
+//		
+//		return (leftDepth > rightDepth) ? leftDepth + 1 : rightDepth + 1;
+		return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+	}
+	
+	// BFS 层序遍历
+	public int maxDepth2(TreeNode root) {
+		if(root == null) return 0;
 		
-		return (leftDepth > rightDepth) ? leftDepth + 1 : rightDepth + 1;
+		@SuppressWarnings("serial")
+		List<TreeNode> queue = new LinkedList<TreeNode>() {
+			{ add(root); }
+		}, temp;
+		/* List<TreeNode> temp; */
+		
+		int res = 0;
+		
+		while(!queue.isEmpty()) {
+			temp = new LinkedList<TreeNode>();
+			
+			for(TreeNode node: queue) {
+				if(node.left != null) temp.add(node.left);
+				if(node.right != null) temp.add(node.right);
+			}
+			
+			queue = temp;
+			res ++;
+		}
+		
+		return res;
 	}
 }
