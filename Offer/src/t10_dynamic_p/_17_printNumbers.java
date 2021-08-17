3063afbaeac6be565f0a93fe1508073f396c6440
liusheng <liushenggcs@163.com>
2021-08-17 12:12:19 +0800

    17_solution2 打印从1到最大的n位数
    处理大数据越界情况，用字符串处理数据

--- Offer/src/t10_dynamic_p/_17_printNumbers.java
+++ Offer/src/t10_dynamic_p/_17_printNumbers.java
@@ -15,4 +15,36 @@ public class _17_printNumbers {
 		
 		return res;
 	}
+	
+	// 方法二： 考虑大数据越界情况，转化为字符数方式
+	int count = 0;
+	int nine = 0;
+	int n, start;
+	int[] res;
+	char[] nums, loop = {'0','1','2','3','4','5','6','7','8','9'};
+	public int[] printNumbers2(int n) {
+		this.n = n;
+		res = new int[(int)Math.pow(10, n) - 1];
+		nums = new char[n];
+		start = n - 1;
+		dfs(0);
+		return res;
+	}
+	private void dfs(int i) {
+		if(i == n) {
+			/* String s = nums.toString().substring(start);  不能用默认的toString，转换的字符串不是自己需要的字符串*/
+			String s = String.valueOf(nums).substring(start);
+			if(!s.equals("0")) res[count ++] = Integer.parseInt(s);
+			if(start + nine == n) start --; // 9，99，999进位
+			return; // 递归终止，不要忘记return
+		}
+		
+		for(char k : loop) {
+			if(k == '9') nine ++; // nine 会影响start，所以要先判断
+			nums[i] = k;
+			dfs(i + 1);
+		}
+		
+		nine --; // 一定要维护nine，只有当9，99，999...时候才进位，其他情况start不变
+	}
 }
